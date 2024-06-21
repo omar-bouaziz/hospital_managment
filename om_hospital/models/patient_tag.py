d040528fc8ad481320ad5f0d392bae6e76bde14e
omar-bouaziz <bouazizomar50@gmail.com>
2024-06-21 13:38:15 +0100

    Copy Function In Odoo

--- om_hospital/models/patient_tag.py
+++ om_hospital/models/patient_tag.py
@@ -7,11 +7,22 @@ class PatientTag(models.Model):
     _description = "  Patient Tag"
 
     name=fields.Char(string="Name",required=True)
-    active=fields.Boolean(string="Active", default=True)
+    active=fields.Boolean(string="Active", default=True , copy=False)
     color=fields.Integer(string="Color")
+    sequence=fields.Integer(string="sequence",default=True)
+
+    @api.returns('self',lambda value:value.id)
+    def copy(self,default=None):
+        if default is None:
+            default={}
+        if not default.get('name'):
+            default['name']=("%s (copy)",self.name)
+        default['sequence'] =10
+        return super(PatientTag,self).copy(default)
 
     _sql_constraints = [
-        ('unique_name', 'unique(name)', 'Name must be unique')
+        ('unique_name', 'unique(name)', 'Name must be unique'),
+        ('unique_sequence', 'unique(sequence>0)', 'sequence must be not zero positive number!')
     ]
 
 
