b41cd1f1b3ab4b48aa9dc2f9a9519a1001c81e24
omar-bouaziz <bouazizomar50@gmail.com>
2024-06-24 10:59:32 +0100

    How To Make Field Required Based On Conditions In Odoo/Label Attribute In Odoo | Class oe_edit_only

--- om_hospital/models/patient.py
+++ om_hospital/models/patient.py
@@ -3,67 +3,64 @@ from datetime import date
 from odoo.exceptions import ValidationError
 
 
-
 class HospitalPatient(models.Model):
     _name = "hospital.patient"
     _inherit = 'mail.thread'
     _description = "hospital patient"
 
-    name = fields.Char(string="Name",tracking=True)
-    date_of_birth=fields.Date(string="date of birth")
-    ref=fields.Char(string="Reference",help="Reference from patient record")
-    age=fields.Integer(string="Age",compute="_compute_age",readonly=True)
+    name = fields.Char(string="Name", tracking=True)
+    date_of_birth = fields.Date(string="date of birth")
+    ref = fields.Char(string="Reference", help="Reference from patient record")
+    age = fields.Integer(string="Age", compute="_compute_age", readonly=True)
     gender = fields.Selection([("male", "Male"), ("female", "Female")], string="Gender")
     active = fields.Boolean(string="Active", default=True)
-    appointement_id=fields.Many2one("hospital.appointement",string="Appointement")
-    image=fields.Image(string="Image")
-    tag_ids=fields.Many2many('patient.tag',string="Tags")
-    appointement_count=fields.Integer(string="Appointement Count", compute="_compute_appointement_count",store=True)
-    appointement_ids= fields.One2many("hospital.appointement",'patient_id',string="Appointement")
-    parent=fields.Char(string="Parent")
-    marital_status=fields.Selection([('married','Married'),('single','Single')],string="Marital Status",tracking=True)
-    partner_name=fields.Char(string="Partner Name")
+    appointement_id = fields.Many2one("hospital.appointement", string="Appointement")
+    image = fields.Image(string="Image")
+    tag_ids = fields.Many2many('patient.tag', string="Tags")
+    appointement_count = fields.Integer(string="Appointement Count", compute="_compute_appointement_count", store=True)
+    appointement_ids = fields.One2many("hospital.appointement", 'patient_id', string="Appointement")
+    parent = fields.Char(string="Parent")
+    marital_status = fields.Selection([('married', 'Married'), ('single', 'Single')], string="Marital Status",
+                                      tracking=True)
+    partner_name = fields.Char(string="Partner Name")
+
     @api.depends('appointement_ids')
     def _compute_appointement_count(self):
-        for rec in self :
-            rec.appointement_count=self.env['hospital.appointement'].search_count([('patient_id','=',rec.id)])
+        for rec in self:
+            rec.appointement_count = self.env['hospital.appointement'].search_count([('patient_id', '=', rec.id)])
 
     @api.constrains('date_of_birth')
     def _check_date_of_birth(self):
-        for rec in self :
+        for rec in self:
             if rec.date_of_birth and rec.date_of_birth > fields.Date.today():
                 raise ValidationError(("the entered date of birth is not acceptable ! "))
 
-
     @api.model
-    def create(self,vals):
-        print("odoo mates",vals)
-        vals['ref']= self.env['ir.sequence'].next_by_code('hospital.patient')
-        return super(HospitalPatient,self).create(vals)
+    def create(self, vals):
+        print("odoo mates", vals)
+        vals['ref'] = self.env['ir.sequence'].next_by_code('hospital.patient')
+        return super(HospitalPatient, self).create(vals)
 
-    def write(self,vals):
+    def write(self, vals):
         if not self.ref and not vals.get('ref'):
             vals['ref'] = self.env['ir.sequence'].next_by_code('hospital.patient')
-        return super(HospitalPatient,self).write(vals)
+        return super(HospitalPatient, self).write(vals)
 
     @api.depends('date_of_birth')
     def _compute_age(self):
-        print("self............",self)
+        print("self............", self)
         for rec in self:
             today = date.today()
-            print("date.today",date.today())
+            print("date.today", date.today())
             if rec.date_of_birth:
                 rec.age = (today.year - rec.date_of_birth.year)
             else:
                 rec.age = 0
 
     def name_get(self):
-       # return [(record.id, "[%s] %s" % (record.ref,record.name))for record in self]
-         patient_list=[]
-         for record in self:
-             name=record.ref + record.name
-             patient_list.append((record.id,name))
-         return patient_list
-
-
-
+        # return [(record.id, "[%s] %s" % (record.ref,record.name))for record in self]
+        patient_list = []
+        for record in self:
+            name = record.ref + record.name
+            patient_list.append((record.id, name))
+        return patient_list
